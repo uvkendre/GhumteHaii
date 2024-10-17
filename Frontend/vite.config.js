35f778f4d18b784736d4d22525f8ef0dc2849677
uvkendre <yuvikendre2002@gmail.com>
2024-10-17 13:58:01 +0530

    Configure GitHub Pages to support SPA routing

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,14 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/GhumteHaii/', // Set the base path for GitHub Pages deployment
+  base: '/GhumteHaii/', // Set the base path for GitHub Pages
   build: {
     outDir: 'dist',
-  },
-  server: {
-    historyApiFallback: true, // Enable fallback for client-side routing
   },
 });
